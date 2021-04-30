40fc482e449c06684fa54f507f58e0bdada4245d
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-04-30 13:32:41 -0300

    Exercicio 5 feito

--- src/challenges.js
+++ src/challenges.js
@@ -22,13 +22,16 @@ function concatName(arrays) {
   let primeiroResultado = arrays[0];
   let divisoria = ', ';
   let ultimoResultado = arrays[arrays.length - 1];
-  let stringResultado = ultimoResultado+divisoria + primeiroResultado;
+  let stringResultado = ultimoResultado + divisoria + primeiroResultado;
   return stringResultado;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontosWins = wins * 3;
+  let pontosTies = ties * 1;
+  let pontosTotais = pontosWins + pontosTies;
+  return pontosTotais;
 }
 
 // Desafio 6
