50cbd8a92d84faa9cde88e4f57750c91b99ac242
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-01 11:06:19 -0300

    Exercicio 7 feito

--- src/challenges.js
+++ src/challenges.js
@@ -49,9 +49,19 @@ function highestCount(arrayNumber) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
+
+  if(distanciaCat1 > distanciaCat2){
+    return 'cat2';
+  } else if(distanciaCat1 < distanciaCat2){
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
+
 
 // Desafio 8
 function fizzBuzz() {
