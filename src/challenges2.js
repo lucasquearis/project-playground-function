0994611a2df89773307638333871819449791d99
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-13 16:45:04 -0300

    tentativa desafio 11 2

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,7 +33,7 @@ function generatePhoneNumber(numeros) {
           }  
         }
       }
-      return '('+numeros[0]+numeros[1]+')'+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+'-'+numeros[7]+numeros[8]+numeros[9]+numeros[10]
+      return '('+numeros[0]+numeros[1]+') '+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+'-'+numeros[7]+numeros[8]+numeros[9]+numeros[10]
     }
   }
 }
