ba3f17e88337f64d6ddc17157101a43e43d8635b
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-16 03:17:26 -0300

    desafio 11 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,26 +16,17 @@ return arrayObj;
 }
 
 // Desafio 11
-function generatePhoneNumber(numeros) {
+function generatePhoneNumber(numeros){
   if(numeros.length != 11){
-    return 'Array com tamanho incorreto.'
-  } else {
-    for(let index = 0; index < numeros.length; index += 1){
-      if(numeros[index] < 0 || numeros[index] > 9){
-        return "não é possível gerar um número de telefone com esses valores"
-      } else {
-        for(let index2 = 1; index2 < numeros.length; index2 += 1){
-          let memory = [];
-          if(numeros[index] === numeros[index2]){
-            memory.push(numeros[index]);
-          } else if(memory.length > 3) {
-            return "não é possível gerar um número de telefone com esses valores"
-          }  
-        }
-      }
-      return '('+numeros[0]+numeros[1]+') '+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+'-'+numeros[7]+numeros[8]+numeros[9]+numeros[10]
+    return "Array com tamanho incorreto."
+  }
+  const numerosSemRepeticao = [...new Set(numeros)]
+  for(let index = 0; index < numeros.length; index += 1){
+    if(numerosSemRepeticao.length <= 9 || numeros[index] > 9 || numeros[index] < 0){
+      return "não é possível gerar um número de telefone com esses valores"
     }
   }
+  return '(' + numeros[0] + numeros [1] + ') ' + numeros[2]+ numeros[3] + numeros[4] + numeros[5] + numeros[6] + '-' + numeros[7] + numeros[8] + numeros[9] + numeros[10]
 }
 // Desafio 12
 function triangleCheck() {
